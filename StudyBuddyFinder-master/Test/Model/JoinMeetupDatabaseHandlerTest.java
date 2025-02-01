86b97133dc8a94723400e93984abd5b74441f681
basoz-lab <64218103+basoz-lab@users.noreply.github.com>
2025-02-01 17:14:26 +0100

    Add files via upload

---
+++ StudyBuddyFinder-master/Test/Model/JoinMeetupDatabaseHandlerTest.java
@@ -0,0 +1,40 @@
+package Model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class JoinMeetupDatabaseHandlerTest {
+/*
+    JoinMeetupDatabaseHandler databaseHandler;
+    Meetup testMeetup;
+    User testUser;
+
+    @Before
+    public void setUp() throws Exception {
+        this.databaseHandler = new JoinMeetupDatabaseHandler();
+        this.testMeetup = new Meetup("testMeetup", "00:00", "2021-05-19", "Datalagring", "testing");
+        this.testUser = new User();
+        testUser.setUsername("fredrik");
+        testUser.setPassword("password1");
+        testUser.setEmail("fp@kth.se");
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.databaseHandler = null;
+        this.testUser = null;
+    }
+
+    @Test
+    public void createNewParticipantMeetup() throws IOException {
+        testMeetup.createNewParticipantMeetup(testMeetup, testUser);
+        assertTrue(testMeetup.meetingExists(testMeetup));
+
+    }*/
+}
---
+++ StudyBuddyFinder-master/Test/Model/MeetingTest.java
@@ -0,0 +1,50 @@
+package Model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class MeetingTest {
+/**
+    JoinMeetupDatabaseHandler databaseHandler;
+    MeetingDatabaseHandler meetupHandler;
+    Meetup testMeetup;
+    Meetings testMeeting;
+    User testUser;
+
+    @Before
+    public void setUp() throws Exception {
+        this.databaseHandler = new JoinMeetupDatabaseHandler();
+        this.meetupHandler = new MeetingDatabaseHandler();
+        this.testMeeting = new Meetings();
+        this.testMeetup = new Meetup("testMeetup", "00:00", "2021-05-19", "Datalagring", "testing");
+        this.testUser = new User();
+        testUser.setUsername("fredrik");
+        testUser.setPassword("password1");
+        testUser.setEmail("fp@kth.se");
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.databaseHandler = null;
+        this.meetupHandler = null;
+        this.testMeeting = null;
+        this.testUser = null;
+    }
+
+    @Test
+    public void createNewMeetup() throws IOException {
+        //testMeeting.createNewMeetup(testMeetup, testUser);
+        meetupHandler.createNewMeetup(testMeetup);
+        assertTrue(meetupHandler.meetingExists(testMeetup));
+    }
+
+    @Test
+    public void createList() {
+    }*/
+}
